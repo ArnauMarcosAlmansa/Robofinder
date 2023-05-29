feb4652625bccc89a93260b5352ebd30e2a2c49d
PerePerez <pereperezlopez@hotmail.com>
2023-05-29 16:47:07 +0200

    I2C por probar Arnau no me mates

--- src/jetson/i2c/I2C.cpp
+++ src/jetson/i2c/I2C.cpp
@@ -46,3 +46,99 @@ void I2C::closeBus() {
         bus = -1;
     }
 }
+
+void I2C::forward(int vel_left, int vel_right, int encoder_value) {
+    uint8_t buffer[5] = {static_cast<uint8_t>(MessageType::FORWARD), static_cast<uint8_t>(vel_left),
+                         static_cast<uint8_t>(vel_right), static_cast<uint8_t>(encoder_value & 0xFF),
+                         static_cast<uint8_t>((encoder_value >> 8) & 0xFF)};
+
+    writeBytes(buffer, sizeof(buffer));
+}
+
+void I2C::backward(int vel_left, int vel_right, int encoder_value) {
+    uint8_t buffer[5] = {static_cast<uint8_t>(MessageType::BACKWARD), static_cast<uint8_t>(vel_left),
+                         static_cast<uint8_t>(vel_right), static_cast<uint8_t>(encoder_value & 0xFF),
+                         static_cast<uint8_t>((encoder_value >> 8) & 0xFF)};
+
+    writeBytes(buffer, sizeof(buffer));
+}
+
+void I2C::left(int vel_left, int vel_right, int encoder_value) {
+    uint8_t buffer[5] = {static_cast<uint8_t>(MessageType::TURN_LEFT), static_cast<uint8_t>(vel_left),
+                         static_cast<uint8_t>(vel_right), static_cast<uint8_t>(encoder_value & 0xFF),
+                         static_cast<uint8_t>((encoder_value >> 8) & 0xFF)};
+
+    writeBytes(buffer, sizeof(buffer));
+}
+
+void I2C::right(int vel_left, int vel_right, int encoder_value) {
+    uint8_t buffer[5] = {static_cast<uint8_t>(MessageType::TURN_RIGHT), static_cast<uint8_t>(vel_left),
+                         static_cast<uint8_t>(vel_right), static_cast<uint8_t>(encoder_value & 0xFF),
+                         static_cast<uint8_t>((encoder_value >> 8) & 0xFF)};
+
+    writeBytes(buffer, sizeof(buffer));
+}
+
+void I2C::stop() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::STOP), sizeof(MessageType::STOP));
+}
+
+void I2C::getEncoderLeft() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::READ_ENCODER_LEFT), sizeof(MessageType::READ_ENCODER_LEFT));
+
+    uint8_t buffer[3];
+    if (!readBytes(buffer, sizeof(buffer)))
+        break;
+
+    channel = buffer[0];
+    message = (buffer[1] << 8) | buffer[2];
+    return (channel, message)
+}
+
+void I2C::getEncoderRight() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::READ_ENCODER_RIGHT), sizeof(MessageType::READ_ENCODER_RIGHT));
+
+    uint8_t buffer[3];
+    if (!readBytes(buffer, sizeof(buffer)))
+        break;
+
+    channel = buffer[0];
+    message = (buffer[1] << 8) | buffer[2];
+    return (channel, message)
+}
+
+void I2C::resetEncoderLeft() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::RESET_ENCODER_LEFT), sizeof(MessageType::RESET_ENCODER_LEFT));
+}
+
+void I2C::resetEncoderRight() {
+    resetEncoderRight(reinterpret_cast<const uint8_t *>(&MessageType::RESET_ENCODER_RIGHT), sizeof(MessageType::RESET_ENCODER_RIGHT));
+}
+
+void I2C::getUltrasonicLeft() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::ULTRASOUND_LEFT), sizeof(MessageType::ULTRASOUND_LEFT));
+
+    uint8_t buffer[3];
+    if (!readBytes(buffer, sizeof(buffer)))
+        break;
+
+    channel = buffer[0];
+    message = (buffer[1] << 8) | buffer[2];
+    return (channel, message)
+}
+
+void I2C::getUltrasonicRight() {
+    writeBytes(reinterpret_cast<const uint8_t *>(&MessageType::ULTRASOUND_RIGHT), sizeof(MessageType::ULTRASOUND_RIGHT));
+
+    uint8_t buffer[3];
+    if (!readBytes(buffer, sizeof(buffer)))
+        break;
+
+    channel = buffer[0];
+    message = (buffer[1] << 8) | buffer[2];
+    return (channel, message)
+}
+
+void I2C::ledOff() {
+    resetEncoderRight(reinterpret_cast<const uint8_t *>(&MessageType::LED_OFF), sizeof(MessageType::LED_OFF));
+}
--- src/jetson/i2c/I2C.h
+++ src/jetson/i2c/I2C.h
@@ -7,13 +7,20 @@
 
 #define ARDUINO_ADDRESS 0x30
 
-
 enum class MessageType : unsigned char
 {
     FORWARD = 11,
     BACKWARD = 12,
     TURN_LEFT = 13,
     TURN_RIGHT = 14,
+    STOP = 15,
+    READ_ENCODER_LEFT = 20,
+    READ_ENCODER_RIGHT = 21,
+    RESET_ENCODER_LEFT = 22,
+    RESET_ENCODER_RIGHT = 23,
+    ULTRASOUND_LEFT = 30,
+    ULTRASOUND_RIGHT = 31,
+    LED_OFF = 40
 };
 
 class I2C {
@@ -28,4 +35,22 @@ public:
     bool readBytes(uint8_t* buffer, size_t size);
     bool writeBytes(const uint8_t* buffer, size_t size);
     void closeBus();
+
+    void forward(int vel_left, int vel_right, int encoder_value);
+    void backward(int vel_left, int vel_right, int encoder_value);
+    void left(int vel_left, int vel_right, int encoder_value);
+    void right(int vel_left, int vel_right, int encoder_value);
+
+    void stop();
+
+    void getEncoderLeft();
+    void getEncoderRight();
+
+    void resetEncoderLeft();
+    void resetEncoderRight();
+
+    void getUltrasonicLeft();
+    void getUltrasonicRight();
+
+    void ledOff();
 };
