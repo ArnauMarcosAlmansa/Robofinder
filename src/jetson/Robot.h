aea58362d5606ccd509b82b2ee7e41a7385dd761
PerePerez <pereperezlopez@hotmail.com>
2023-05-31 11:31:48 +0200

    Robot.cpp añadida las funciones de conversion

--- src/jetson/Robot.h
+++ src/jetson/Robot.h
@@ -1,11 +1,21 @@
 #include <opencv2/core/core.hpp>
 
+#define WHEEL_PERIMETER 0.132       // Permimetro de la rueda.
+#define PULSES_FOR_REVOLUTION 12    // Pulsos del encoder
+#define WHEEL_DISTANCE 0.22         // Ancho entre ruedas.
+#define PI 3.14159265359
+
 class Robot
 {
 public:
     Robot();
     void turn(float radians);
     void move(float meters);
+
+    float pulses_to_meters(int pulses);
+    int meters_to_pulses(float meters);
+
+    std::pair<bool, int> rad_to_pulses(float rad);
 
     cv::Mat& get_position();
     cv::Mat& get_orientation();
